1e7ff2972e806e35c30a1dcbad01ce272ab7667c
Bhumika-Kothwal <kothwalbhumika@gmail.com>
2020-06-08 18:30:23 +0530

    old file deleted

--- bot_with_90_turn_ and_sensors/src/sensor_ke_saath.c
+++ bot_with_90_turn_ and_sensors/src/sensor_ke_saath.c
@@ -12,9 +12,10 @@ int main(int argc,char* argv[])
     int rightMotorHandle;  
     int botbase;
 	int reference;
+	int sensor[4];
     // number of variables passed from lua to this code + 1
     // (+1 because the first value in argv is always the location of this program)
-    int n = 6;
+    int n = 10;
 
     // save the handles
     if (argc>=n){
@@ -23,6 +24,10 @@ int main(int argc,char* argv[])
         rightMotorHandle = atoi(argv[3]);
 		botbase = atoi(argv[4]);
 		reference = atoi(argv[5]);
+		sensor[0] = atoi(argv[6]);
+		sensor[1] = atoi(argv[7]);
+		sensor[2] = atoi(argv[8]);
+		sensor[3] = atoi(argv[9]);
     }
     else{
         printf("Sufficient arguments not provided'!\n");
@@ -37,7 +42,8 @@ int main(int argc,char* argv[])
     float* auxValues = NULL;
     int* auxValuesCount = NULL;
 	char s;
-	
+	int c=0;
+	float svalue[4];
     // for details on these functions refer: https://www.coppeliarobotics.com/helpFiles/en/remoteApiFunctions.htm
 
     // start communication with coppeliasim
@@ -51,11 +57,34 @@ int main(int argc,char* argv[])
 		printf("motion type:\n");
 		scanf("%c",&s);
 		while (simxGetConnectionId(clientID)!=-1){
-
+			//getting orientation of bot with respect to reference frame
 			simxGetObjectOrientation(clientID, botbase, reference, angle, simx_opmode_streaming);
 			if( simxGetObjectOrientation(clientID, botbase, reference, angle, simx_opmode_buffer) == simx_return_ok)
                 		printf("the angle: %f %f %f\n",angle[0],angle[1],angle[2]);
-
+                		
+            //reading sensors
+			simxReadVisionSensor(clientID, sensor[0], NULL, &auxValues, &auxValuesCount,simx_opmode_streaming);
+			if( simxReadVisionSensor(clientID, sensor[0], NULL, &auxValues, &auxValuesCount,simx_opmode_buffer) == simx_return_ok){
+               // printf("sensor: %f\n", auxValues[10]);
+               svalue[0] = auxValues[10];
+            }
+			simxReadVisionSensor(clientID, sensor[1], NULL, &auxValues, &auxValuesCount,simx_opmode_streaming);
+			if( simxReadVisionSensor(clientID, sensor[1], NULL, &auxValues, &auxValuesCount,simx_opmode_buffer) == simx_return_ok){
+               // printf("sensor: %f\n", auxValues[10]);
+               svalue[1] = auxValues[10];
+            }
+			simxReadVisionSensor(clientID, sensor[2], NULL, &auxValues, &auxValuesCount,simx_opmode_streaming);
+			if( simxReadVisionSensor(clientID, sensor[2], NULL, &auxValues, &auxValuesCount,simx_opmode_buffer) == simx_return_ok){
+               // printf("sensor: %f\n", auxValues[10]);
+               svalue[2] = auxValues[10];
+            }
+			simxReadVisionSensor(clientID, sensor[3], NULL, &auxValues, &auxValuesCount,simx_opmode_streaming);
+			if( simxReadVisionSensor(clientID, sensor[3], NULL, &auxValues, &auxValuesCount,simx_opmode_buffer) == simx_return_ok){
+               // printf("sensor: %f\n", auxValues[10]);
+               svalue[3] = auxValues[10];
+            }
+            printf("the sensor values: %f %f %f %f\n",svalue[0],svalue[1],svalue[2],svalue[3]);
+            
 			if(s=='f')
         		move(clientID,leftMotorHandle,rightMotorHandle,leftMotorSpeed,rightMotorSpeed);
         	else if(s=='b')
@@ -63,19 +92,30 @@ int main(int argc,char* argv[])
         	else if(s=='l'){
 				if(angle[1]< 1.5 )
 					move(clientID,leftMotorHandle,rightMotorHandle,-1*leftMotorSpeed,rightMotorSpeed);
-				else
+				else{
+					//c = 1;
 					move(clientID,leftMotorHandle,rightMotorHandle,0,0);
+				}
 			}
         	else if(s=='r'){
         		if(angle[1]> -1.5 )
 					move(clientID,leftMotorHandle,rightMotorHandle,leftMotorSpeed,-1*rightMotorSpeed);
-				else
+				else{
+					//c =1;
 					move(clientID,leftMotorHandle,rightMotorHandle,0,0);
+				}
         	}
         	else{ 
         		printf("invalid input\n");
         		break;
         	}
+        	/*if(c==1)
+        	{
+        		simxSetObjectOrientation(clientID,reference,botbase,angle,simx_opmode_oneshot);
+				printf("reference orientation: ");
+				printf("the angle: %f %f %f\n",angle[0],angle[1],angle[2]);
+				c =0;
+        	}*/
         	extApi_sleepMs(25);
         	        /*********************************************************/
         }
